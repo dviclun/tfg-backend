3d5cf1d6eb3dd678f6173bcafe45858185096d8a
Daniel Vicent <daniel.vicent.luna@gmail.com>
2024-04-25 12:10:26 +0200

    get videos controllers added

--- src/controllers/trainings.controllers.js
+++ src/controllers/trainings.controllers.js
@@ -12,3 +12,46 @@ export const getMuscularGroups = async (req, res) => {
         })
     }
 }
+
+export const getArmsVideos = async (req, res) => {
+    try {
+        const [result] = await conexion.query("SELECT * FROM tfg_training_videos WHERE muscular_group IN (3, 6, 7, 8)");
+        console.log(result);
+        res.status(200).json(result);
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({
+            message: "Error en el servidor"
+        })
+    }
+}
+
+export const getLegsVideos = async (req, res) => {
+    try {
+        const [result] = await conexion.query("SELECT * FROM tfg_training_videos WHERE muscular_group IN (4, 9, 10, 11, 12)");
+        console.log(result);
+        res.status(200).json(result);
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({
+            message: "Error en el servidor"
+        })
+    }
+}
+
+
+export const getVideosByGroup = async (req, res) => {
+    try {
+        
+        const { muscular_group } = req.params
+        const [result] = await conexion.query("SELECT * FROM tfg_training_videos WHERE muscular_group = ?", [muscular_group]);
+
+        res.status(200).json(result); //la  respuesta que devuelve el servidor
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Error en el servidor"
+        })
+    }
+
+};
--- src/routes/trainings.routes.js
+++ src/routes/trainings.routes.js
@@ -1,10 +1,15 @@
 "use strict"
 import { Router } from 'express'
-import { getMuscularGroups } from '../controllers/trainings.controllers.js';
+import { getArmsVideos, getLegsVideos, getMuscularGroups, getVideosByGroup } from '../controllers/trainings.controllers.js';
 
 const trainingsRouter = Router();
 
 trainingsRouter.get("/muscularGroups", getMuscularGroups);
+trainingsRouter.get("/armsVideos", getArmsVideos);
+trainingsRouter.get("/legsVideos", getLegsVideos);
+trainingsRouter.get("/videos/:muscular_group", getVideosByGroup);
+
+
 
 
 
