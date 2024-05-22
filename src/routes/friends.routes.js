02db70e314ea5d360d64f9e583089d0ca2797ba8
Daniel Vicent <daniel.vicent.luna@gmail.com>
2024-05-22 14:51:14 +0200

    Create friend request and Delete friend enpoits and routes created

--- src/routes/friends.routes.js
+++ src/routes/friends.routes.js
@@ -1,10 +1,13 @@
 "use strict"
 import { Router } from 'express';
-import { getAllFriends, getNonFriendsByUsername } from '../controllers/friends.controller.js';
+import { createFriendRequest, deleteFriend, getAllFriends, getFriendRequests, getNonFriendsByUsername } from '../controllers/friends.controller.js';
 
 const friendsRouter = Router();
 
 friendsRouter.post("/allFriends", getAllFriends);
 friendsRouter.post("/getNonFriendsByUsername", getNonFriendsByUsername);
+friendsRouter.post("/getFriendRequests", getFriendRequests);
+friendsRouter.post("/createFriendRequest", createFriendRequest);
+friendsRouter.delete("/deleteFriend", deleteFriend);
 
 export default friendsRouter;
