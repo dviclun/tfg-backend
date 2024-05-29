6439da77d4e8bb6406d17005e225471ab247ac6a
Daniel Vicent <daniel.vicent.luna@gmail.com>
2024-05-29 11:11:49 +0200

    Web Socket implemented for in real time chat

--- src/app.js
+++ src/app.js
@@ -6,18 +6,29 @@ import trainingsRouter from './routes/trainings.routes.js';
 import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { Server } from 'socket.io';
 
 import { PORT } from './config.js'
 import entriesRouter from './routes/entries.routes.js';
 import friendsRouter from './routes/friends.routes.js';
 import chatRouter from './routes/chat.routes.js';
+import { createServer } from 'http';
 //import './config.js'
 
 // Obtener __dirname en un módulo ES
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+
+
 const app = express(); //creado el objeto con la instacia de express
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        credentials: true
+    }
+});
 
 //habilitar CORS
 app.use(cors());
@@ -39,6 +50,26 @@ app.use((req, res) => {
         message: "endpoint no encontrado"
     })
 })
-app.listen(PORT, () => {
+
+server.listen(PORT, () => {
     console.log('escuchando solicitud');
 })
+const onlineUsers = new Map();
+
+io.on('connection', (socket) => {
+    
+    socket.on("add-user", (user_id)=> {
+        onlineUsers.set(user_id, socket.id)
+    })
+
+    socket.on("send-msg", (data) => {
+        const sendUserSocket = onlineUsers.get(data.user_to);
+
+        if(sendUserSocket) {
+            socket.to(sendUserSocket).emit("msg-receive", data)
+        }
+    })
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+      });
+});
