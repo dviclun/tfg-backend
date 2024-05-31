8a9e3f5a9ff21ad56ebee9d41738a311de063b06
Daniel Vicent <daniel.vicent.luna@gmail.com>
2024-05-31 12:56:59 +0200

    Controllers and routes added for personalized training requests

--- src/routes/requests.routes.js
+++ src/routes/requests.routes.js
@@ -1,11 +1,14 @@
 "use strict"
 import { Router } from 'express';
-import { getRequestResponse, getRequestsByUserId, sendRequest } from '../controllers/requests.controller.js';
+import { createTrainingResponse, editRequestStatus, getAllPendingRequests, getRequestResponse, getRequestsByUserId, sendRequest } from '../controllers/requests.controller.js';
 
 const requestsRouter = Router();
 
 requestsRouter.post("/sendRequest", sendRequest);
 requestsRouter.post("/getRequestsByUserId", getRequestsByUserId);
 requestsRouter.post("/getRequestResponse", getRequestResponse);
+requestsRouter.get("/getAllPendingRequests", getAllPendingRequests);
+requestsRouter.post("/createTrainingResponse", createTrainingResponse);
+requestsRouter.put("/updateRequestStatus", editRequestStatus);
 
 export default requestsRouter;
