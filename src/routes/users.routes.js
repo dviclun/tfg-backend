68e95efd6710753b5136cbb22edc1f86f275e476
Daniel Vicent <daniel.vicent.luna@gmail.com>
2024-06-04 13:19:03 +0200

    multer middleware added without writing file

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -6,13 +6,6 @@ import fs from 'fs';
 import { registerUserValidator, updateBiographyValidator, updateUserPointsValidator } from '../validators/users.validators.js';
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        const uploadDir = 'uploads/';
-        if (!fs.existsSync(uploadDir)) {
-        fs.mkdirSync(uploadDir);
-        }
-        cb(null, uploadDir);
-      },
       filename: function (req, file, cb) {
         cb(null, file.originalname) // Nombre original del archivo
       }
@@ -32,7 +25,7 @@ usersRouter.post("/registerUser", registerUserValidator, registerUser);
 usersRouter.put("/updatePoints", updateUserPointsValidator, updateUserPoints);
 usersRouter.post('/verifyCaptcha', verifyCaptcha);
 usersRouter.put("/updateBio", updateBiographyValidator,updateBiography);
-usersRouter.post("/uploadUserImage", uploadUserImage);
+usersRouter.post("/uploadUserImage",upload.single('image'), uploadUserImage);
 usersRouter.post("/getUserProfileImage", getProfileImage);
 usersRouter.post("/updateUserRole", updateUserRole);
 
